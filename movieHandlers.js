f48547b63b2a53a457ec1f8bb8ffa1d46a886ca1
Theo Rodriguez <rodriguez.theo026@gmail.com>
2023-04-24 17:00:17 +0200

    First commit

--- movieHandlers.js
+++ movieHandlers.js
@@ -1,3 +1,5 @@
+const database = require('./database');
+
 const movies = [
   {
     id: 1,
@@ -25,20 +27,67 @@ const movies = [
   },
 ];
 
+const users = [
+    {
+      id: 1,
+      firstname: "John",
+      lastname: "Doe",
+      mail: "john.doe@example.com",
+    },
+    {
+      id: 2,
+      firstname: "Jane",
+      lastname: "Doe",
+      mail: "jane.doe@example.com",
+    },
+    {
+      id: 3,
+      firstname: "Bob",
+      lastname: "Smith",
+      mail: "bob.smith@example.com",
+    },
+    {
+      id: 4,
+      firstname: "Alice",
+      lastname: "Jones",
+      mail: "alice.jones@example.com",
+    },
+    {
+      id: 5,
+      firstname: "James",
+      lastname: "Brown",
+      mail: "james.brown@example.com",
+    },
+];
+
 const getMovies = (req, res) => {
-  res.json(movies);
+  database
+    .query("select * from movies")
+    .then(([movies]) => {
+      res.json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
 };
 
 const getMovieById = (req, res) => {
   const id = parseInt(req.params.id);
 
-  const movie = movies.find((movie) => movie.id === id);
-
-  if (movie != null) {
-    res.json(movie);
-  } else {
-    res.status(404).send("Not Found");
-  }
+  database
+    .query("select * from movies where id = ?", [id])
+    .then(([movies]) => {
+      if (movies[0] != null) {
+        res.json(movies[0]);
+      } else {
+        res.status(404).send("Not Found");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
 };
 
 module.exports = {
