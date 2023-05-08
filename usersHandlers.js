65d6c4dce33f19e381ef35b07c6057810d0a3396
Theo Rodriguez <rodriguez.theo026@gmail.com>
2023-05-08 18:51:55 +0200

    Adding routes with query for movies and users

--- usersHandlers.js
+++ usersHandlers.js
@@ -1,15 +1,36 @@
 const database = require('./database');
 
 const getUsers = (req, res) => {
-  database
-    .query("select * from users")
-    .then(([users]) => {
-      res.json(users);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error retrieving data from database");
-    });
+  const initialSql = "select * from users";
+  const where = [];
+
+  if(req.query.language != null){
+    where.push({
+        column: 'language',
+        value: req.query.language,
+        operator: '=',
+      });
+  }
+
+  if(req.query.city != null){
+    where.push({
+      column: 'city',
+      value: req.query.city,
+      operator: '=',
+    });  
+  }
+
+  database.query(
+    where.reduce((sql, {column, operator}, index) => `${sql} ${index === 0 ? "where" : "and"} ${column} ${operator} ?`, initialSql),
+    where.map(({value}) => value)
+  )
+  .then(([result]) => {
+    res.json(result)
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send("Error retrieving data from users");
+  });
 };
 
 const getUsersById = (req, res) => {
